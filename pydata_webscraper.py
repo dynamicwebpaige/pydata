832682b7fe10f6cb0165f087768cde0455148d9f
Paige Bailey <profoundlypaige@gmail.com>
2015-04-22 09:51:30 -0500

    Update pydata_webscraper.py

--- pydata_webscraper.py
+++ pydata_webscraper.py
@@ -6,14 +6,10 @@ import csv
 import URL_generator as populator
 from bs4 import BeautifulSoup
 
-police_districts = ['populate w/ police districts #s, there should be 90']
-years_abbr = ['09','10','11','12','13','14','15']
-years_stats = ['stats2009','stats2010','stats2011','stats2012','stats2013','stats2014','stats2015']
-months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
-
 # URL's 
 url = 'http://www.houstontx.gov/police/cs/stats2015/jan15/jan1510h40.htm'
-# looping through list of URL's 
+
+# next step: looping through list of URL's produced by URL_generator.py 
 # urls = populator.URL_populator()
 
 html = urllib2.urlopen(url).read()
@@ -35,7 +31,7 @@ for row in soup('table', {'class':'MsoNormalTable'}):
 
 # pprint.pprint(list(chunks(holder,10)))
 holder2 = list(chunks(holder,10))
-csvfile = '/home/paige/list_of_crimes.csv'
+csvfile = '/home/paige/pydata/list_of_crimes.csv'
 
 with open(csvfile, "w") as output:
 	writer = csv.writer(output, lineterminator='\n')
